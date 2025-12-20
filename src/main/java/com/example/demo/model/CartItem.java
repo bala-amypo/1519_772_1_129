99c82fb8479ceef123a9c9ec53d643f331d7988a
bala-amypo <bala@amypo.in>
2025-12-20 11:39:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/CartItem.java
@@ -0,0 +1,49 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Cart cart;
+
+    @ManyToOne(optional = false)
+    private Product product;
+
+    private Integer quantity;
+
+    // getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
