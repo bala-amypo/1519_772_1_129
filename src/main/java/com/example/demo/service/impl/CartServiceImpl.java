527c0a61e23eb10b26c8a42c85a08af7cd621cb2
bala-amypo <bala@amypo.in>
2025-12-24 08:35:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/CartServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Cart;
+import com.example.demo.repository.CartRepository;
+import com.example.demo.service.CartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl implements CartService {
+
+    private final CartRepository cartRepository;
+
+    public CartServiceImpl(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    @Override
+    public Cart createCart(Long userId) {
+
+        cartRepository.findByUserId(userId)
+                .ifPresent(cart -> {
+                    throw new IllegalArgumentException("Cart already exists");
+                });
+
+        Cart cart = new Cart();
+        cart.setUserId(userId);
+
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public Cart getCartById(Long id) {
+        return cartRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cart not found"));
+    }
+
+    @Override
+    public Cart getCartByUserId(Long userId) {
+        return cartRepository.findByUserId(userId)
+                .orElseThrow(() -> new RuntimeException("Cart not found"));
+    }
+
+    @Override
+    public void deactivateCart(Long id) {
+        Cart cart = getCartById(id);
+        cartRepository.delete(cart);
+    }
+}
