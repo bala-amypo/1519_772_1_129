1020a7e612d55664341ed2a9824241e0349b1f3b
bala-amypo <bala@amypo.in>
2025-12-24 06:21:57 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 @Entity
 @Table(
@@ -17,50 +18,74 @@ public class Product {
     @Column(nullable = false, unique = true)
     private String sku;
 
+    @Column(nullable = false)
     private String name;
+
+    private String category;
 
     @Column(nullable = false)
     private BigDecimal price;
 
     private Boolean active = true;
 
+    private Timestamp createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+    }
+
+
+
     public Long getId() {
         return id;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public Boolean getActive() {
-        return active;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getSku() {
+        return sku;
+    }
+
     public void setSku(String sku) {
         this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
     public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
+    public Boolean getActive() {
+        return active;
+    }
+
     public void setActive(Boolean active) {
         this.active = active;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
     }
 }
